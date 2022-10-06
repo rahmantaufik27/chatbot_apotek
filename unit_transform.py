2ed5cb870b08f415c6aa1f79bcd72ef9f278bd9e
nafnaufal <naufalanbial@gmail.com>
2022-10-06 14:28:28 +0700

    Product Name Transform

--- unit_transform.py
+++ unit_transform.py
@@ -1,11 +1,15 @@
 import re
 
 """
+Unit Transform
     1. replace gr/g => gram, mg => miligram, l => liter, ml => mililiter
     2. replace unit jika sebelum unit terdapat angka/spasi dan setelah unit terdapat spasi/titik/end.
     3. tidak mengganti unit pada awal kalimat
 
-    contoh ada pada fungsi main
+Product Name Transform
+    1. menambahkan spasi pada nama produk seperti 'erha4' menjadi 'erha 4'
+    
+contoh ada pada fungsi main
 """
 
 def unit_transform(str):
@@ -20,10 +24,15 @@ def unit_transform(str):
         str = re.sub(key, value, str)
     return (str)
 
+def product_name_transform(str):
+    str = re.sub("(?<=[A-Za-z])+[0-9]+(?=\s|\.|$)",
+                 lambda group: " "+group[0], str)
+    return str
 
-def main():    
-    x = "g Berikut melalui 4mg 5 gr 5l l Informasig l gmengenai lalu *ACNE TONER 2 (AT 2)* 60 g. asdAdasd g"
-    print(unit_transform(x))
+def main():
+    x = "erha4 g erha4 Berikut er4h 4rh melalui 4mg 5 gr 5l l Informasig l gmengenai lalu *ACNE TONER 2 (AT 2)* 60 g. asdAdasd g erha4"
+    print(x)
+    print(product_name_transform(x))
 
 
 if __name__ == "__main__":
